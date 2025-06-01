d4b6fb723702ce24faa4e091a77ad5697e109768
prakesh2539@gmail.com <prakesh2539@gmail.com>
2025-06-01 20:20:41 +0530

    new class and prious class data changed

---
+++ src/test/java/testcases/Tc003_PaymentTest.java
@@ -0,0 +1,14 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+import base.BaseClass;
+
+public class Tc003_PaymentTest extends BaseClass {
+	
+	@Test
+	public void paymentTest() {
+		System.out.println("payment test successfully executed");
+	}
+
+}
